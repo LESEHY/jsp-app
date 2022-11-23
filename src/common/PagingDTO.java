40a0eea90ac0531847dc81055ceef84e3cfe5759
LESEHY <tmdgus2496@gmail.com>
2022-11-23 14:01:41 +0900

    ss

--- src/common/PagingDTO.java
+++ src/common/PagingDTO.java
@@ -17,11 +17,9 @@ public class PagingDTO {
 	private int listGroup;
 	// 5.남은 레코드수 : 리스트 그룹에서 남은 레코드수
 	private int etcRecord;
-	// 6.페이징링크 코드 저장변수
-	private String pgCode;
-	// 파라미터 형변환 변수(현재 페이지번호)
+	// 6.파라미터 형변환 변수(현재 페이지번호)
 	private int pageSeq; // 기본값 1(파라미터가 없으면 1들어감!)
-	// 한계수 체크: 나머지가 있고 없고에 따라 1개차이남
+	// 7. 한계수 체크: 나머지가 있고 없고에 따라 1개차이남
 	private int limit; 
 	
 	// 겟터 셋터 생성
@@ -31,6 +29,13 @@ public class PagingDTO {
 	}
 	public void setStartNum(int startNum) {
 		this.startNum = startNum;
+	}
+	
+
+	// 2.페이지당 레코드개수 : LIMIT의 개수
+	// -> final이므로 셋터가 없다!
+	public int getOnePageCnt() {
+		return onePageCnt;
 	}
 	
 	// 3.전체 레코드수
@@ -57,15 +62,7 @@ public class PagingDTO {
 		this.etcRecord = etcRecord;
 	}
 	
-	// 6.페이징링크 코드 저장변수
-	public String getPgCode() {
-		return pgCode;
-	}
-	public void setPgCode(String pgCode) {
-		this.pgCode = pgCode;
-	}
-	
-	// 파라미터 형변환 변수(현재 페이지번호)
+	// 6.파라미터 형변환 변수(현재 페이지번호)
 	public int getPageSeq() {
 		return pageSeq;
 	}
@@ -73,7 +70,7 @@ public class PagingDTO {
 		this.pageSeq = pageSeq;
 	}
 	
-	// 한계수 체크: 나머지가 있고 없고에 따라 1개차이남
+	// 7.한계수 체크: 나머지가 있고 없고에 따라 1개차이남
 	public int getLimit() {
 		return limit;
 	}
@@ -81,11 +78,6 @@ public class PagingDTO {
 		this.limit = limit;
 	}
 	
-	// 2.페이지당 레코드개수 : LIMIT의 개수
-	// -> final이므로 셋터가 없다!
-	public int getOnePageCnt() {
-		return onePageCnt;
-	}
 
 
 }

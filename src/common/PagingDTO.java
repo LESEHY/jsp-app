d1bc5275654259850db2061d791a5acc5e12e8d1
LESEHY <tmdgus2496@gmail.com>
2022-11-23 11:55:10 +0900

    ss

--- src/common/PagingDTO.java
+++ src/common/PagingDTO.java
@@ -8,16 +8,16 @@ public class PagingDTO {
 	// 2.페이지당 레코드개수 : LIMIT의 개수
 	private int onePageCnt = 3;
 	// 3.전체 레코드수
-	int totalCnt = 0;
+	private int totalCnt = 0;
 	// 4.리스트 그룹수 : 전체개수 ÷ 페이지당개수
-	int listGroup = 0;
+	private int listGroup = 0;
 	// 5.남은 레코드수 : 리스트 그룹에서 남은 레코드수
-	int etcRecord = 0;
+	private int etcRecord = 0;
 	// 6.페이징링크 코드 저장변수
-	String pgCode = "";
+	private String pgCode = "";
 	// 파라미터 형변환 변수(현재 페이지번호)
-	public int pageSeq = 1; // 기본값 1(파라미터가 없으면 1들어감!)
+	private int pageSeq = 1; // 기본값 1(파라미터가 없으면 1들어감!)
 	// 한계수 체크: 나머지가 있고 없고에 따라 1개차이남
-	int limit; 
+	private int limit; 
 
 }
